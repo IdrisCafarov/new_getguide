014c9c9ee7c2e5877831d5011ee6868651c1a653
IdrisCafarov <idris.ceferov244@gmail.com>
2023-06-06 14:32:00 +0400

    changes

--- app/product/urls.py
+++ app/product/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', index, name='index'),
     path('faq/', Faq, name='faq'),
     path('about/',About,name="about"),
-    path('blogs',BlogView,name="blogs"),
-    path('search_results',SearchResults,name="search_results"),
+    path('blogs/',BlogView,name="blogs"),
+    path('search_results/',SearchResults,name="search_results"),
 ]
